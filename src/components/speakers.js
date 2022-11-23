de31b8d15fa9cce9583f5f0fab5d830dcbcf7330
juandapps <juandapps@hotmail.com>
2022-11-22 22:10:23 -0600

    speaker card

---
+++ src/components/speakers.js
@@ -0,0 +1,44 @@
+const speakersData = [
+  {
+    name: 'Alberto Viera',
+    title: '',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti a dolorum natus! Tenetur iure soluta consequatur repellendus omnis, nisi ea.',
+    pfp: '',
+  },
+  {
+    name: 'Anett Rolikova',
+    title: '',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti a dolorum natus! Tenetur iure soluta consequatur repellendus omnis, nisi ea.',
+    pfp: '',
+  },
+  {
+    name: 'Kevin Owocki',
+    title: '',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti a dolorum natus! Tenetur iure soluta consequatur repellendus omnis, nisi ea.',
+    pfp: '',
+  },
+  {
+    name: 'Justyna Broniszewska',
+    title: '',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti a dolorum natus! Tenetur iure soluta consequatur repellendus omnis, nisi ea.',
+    pfp: '',
+  },
+  {
+    name: 'Mudit Gupta',
+    title: '',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti a dolorum natus! Tenetur iure soluta consequatur repellendus omnis, nisi ea.',
+    pfp: '',
+  },
+  {
+    name: 'Pablo Sabbatella',
+    title: '',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti a dolorum natus! Tenetur iure soluta consequatur repellendus omnis, nisi ea.',
+    pfp: '',
+  },
+];
